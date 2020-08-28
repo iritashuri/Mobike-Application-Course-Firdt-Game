b076ead4ce682b7a68c8b2db650473fa1daa99c4
iritashuri <iritashuri5109@gmail.com>
2020-08-28 11:26:02 +0300

    upload project

--- app/src/main/java/com/example/fistassigment/Activity_Play.java
+++ app/src/main/java/com/example/fistassigment/Activity_Play.java
@@ -3,7 +3,6 @@ package com.example.fistassigment;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
-import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
